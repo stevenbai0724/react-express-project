78c610500106ebaabb5ed893487aba4e4ae733b6
hw2zheng <haowei.zheng12@gmail.com>
2021-09-06 14:10:13 -0700

    Add more sample calls

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,7 +1,7 @@
 import './customers.css';
-import react, {Component} from 'react'
+import react, { Component } from 'react'
 
-class Customers extends Component{
+class Customers extends Component {
 
     constructor() {
         super();
@@ -14,23 +14,39 @@ class Customers extends Component{
     componentDidMount() {
         fetch('/api/customers')
             .then(res => res.json())
-            .then(customers => this.setState({customers}, () => console.log('Customers fetched..', {customers}) ))
+            .then(customers => this.setState({ customers }, () => console.log('Customers fetched..', { customers })))
+
+        // Sample post request
+        // Requires application/json header to be parsed correctly
+        // postBody is sent in as the body param
+        // requires JSON.stringify
+        const postBody = {
+            customers: [
+                { id: 1, firstName: "Da", lastName: "Baby" },
+                { id: 2, firstName: "John", lastName: "Cena" },
+                { id: 3, firstName: "Pepe", lastName: "Frog" },
+            ]
+        }
+        fetch('/api/customers', {
+            method: "POST", body: JSON.stringify(postBody),
+            headers: { 'Content-Type': 'application/json' }
+        })
     }
 
     render() {
-    return (
-        <div >
-        <h2>Customer List</h2>
+        return (
+            <div >
+                <h2>Customer List</h2>
 
-        <ul>
-            {this.state.customers.map(customer =>   
-                <li  key={customer.id}> {customer.firstName}  {customer.lastName}</li>
+                <ul>
+                    {this.state.customers.map(customer =>
+                        <li key={customer.id}> {customer.firstName}  {customer.lastName}</li>
 
-            )}
-        </ul>
-        
-        </div>
-    );
+                    )}
+                </ul>
+
+            </div>
+        );
 
     }
 }
--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const axios = require('axios')
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
+// Use this to parse body params from POST requests
+app.use(express.json());
+
+// async / await pattern
+app.get('/api/customers', async (req, res) => {
     const customers = [
         {id:1, firstName: "Da", lastName: "Baby"},
         {id:2, firstName: "John", lastName: "Cena"},
         {id:3, firstName: "Pepe", lastName: "Frog"},
-
     ];
+
+    // PUT API KEY HERE
+    const API_KEY = ""
+    const requestOptions = {
+        headers: { 
+            Authorization: `Bearer ${API_KEY}`
+        },
+    }
+
+    // DMOJ application call
+    const res2 = await axios.get('https://dmoj.ca/api/v2/contests?tag=seasonal&tag=dmopc', requestOptions)
+    // All data stored under res2.data...
+    console.log(JSON.stringify(res2.data.data.objects[0], null, 2))
 
     res.json(customers);
 })
+
+app.post('/api/customers', async (req, res) => {
+    // Uses req.body stores the "body" parameter passed in by fetch
+    console.log(req.body)
+    res.json({ test: "test" });
+})
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
